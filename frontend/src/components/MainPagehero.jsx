280195b4109c3073cb2d265e027ec2bc5f99f2a6
Kamil Głuchowski <kamilkamil5542@wp.pl>
2022-12-23 16:26:16 +0100

    feat: add emlem & flex to grid above mobile-view

--- frontend/src/components/MainPagehero.jsx
+++ frontend/src/components/MainPagehero.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import Logo from '../assets/imgs/logo.png'
+import Emblem from '../assets/imgs/emblem.png'
 import Westerplatte from '../assets/imgs/westerplatte.jpg'
 
 import { FadeIn } from 'react-slide-fade-in'
@@ -7,7 +9,7 @@ import { FadeIn } from 'react-slide-fade-in'
 function MainPagehero() {
 	return (
 		<>
-			<section className="pagehero" style={{ paddingTop: '2rem' }}>
+			<section className="pagehero">
 				<FadeIn
 					from="top"
 					positionOffset={20}
@@ -18,29 +20,29 @@ function MainPagehero() {
 						<img
 							src={Logo}
 							alt="Zespół Szkół Elektronicznych Im. Westerplatte"
-							height={150}
+							height={window.innerWidth >= 1024 ? 175 : 150}
 							className="tablet"
 						/>
 
-						<h1 className="pagehero__text">
-							Zespół Szkół Elektronicznych
-							<br />
-							Im. Westerplatte
-							<br />W Radomiu
-						</h1>
+						<div className="flex-center">
+							<img src={Emblem} alt="" height={100} />
+							<h1 className="pagehero__text">
+								Zespół Szkół Elektronicznych
+								<br />
+								Im. Westerplatte
+								<br />W Radomiu
+							</h1>
+						</div>
 
 						<img
 							src={Westerplatte}
 							alt="Bohaterowie Westerplatte"
-							width={250}
-							height={175}
+							width={window.innerWidth >= 1024 ? 220 : 180}
+							height={window.innerWidth >= 1024 ? 150 : 120}
 							style={{ borderRadius: '8px' }}
 						/>
 					</div>
 				</FadeIn>
-			</section>
-
-			<section className="pagehero">
 				<h2 className="quote">
 					<FadeIn
 						from="left"
@@ -49,7 +51,13 @@ function MainPagehero() {
 						delayInMilliseconds={1000}
 					>
 						<div className="pagehero__content">
-							<figure style={{ marginLeft: 'auto' }}>
+							<figure
+								style={{
+									marginLeft: 'auto',
+									placeSelf: '',
+									gridColumn: '2 / span 3',
+								}}
+							>
 								<blockquote>
 									<q>
 										Lorem ipsum dolor, sit amet consectetur
@@ -67,6 +75,10 @@ function MainPagehero() {
 					</FadeIn>
 				</h2>
 			</section>
+
+			{/* <section className="pagehero">
+
+			</section> */}
 		</>
 	)
 }
